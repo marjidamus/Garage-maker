dd7c6d900b830b27e04762c26f629877fedea447
Maria Stoycheva <mariq.radostinova_2001@abv.bg>
2021-04-11 12:17:56 +0300

    initialise the functions

---
+++ Homework/MyString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "MyString.h"
+#include <cstring>
+#include <assert.h>
+
+MyString::MyString()
+{
+    this->string=nullptr;
+    this->length=0;
+}
+
+MyString::MyString(const char* str)
+{
+    
+}
