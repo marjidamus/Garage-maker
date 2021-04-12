02238c20f30175865a90bd1e2390d16f66b6398a
BlagovestMihaylov <BlagovestMihaylov@abv.bg>
2021-04-12 21:52:19 +0300

    problems with declarations

--- Homework/MyString.cpp
+++ Homework/MyString.cpp
@@ -1,15 +1,165 @@
 #include <iostream>
 #include "MyString.h"
 #include <cstring>
-#include <assert.h>
+#include <cassert>
+
+void MyString::deallocate()
+{
+    if (this->string)
+    {
+        delete[] this->string;
+    }
+}
+
+char *MyString::dynString(const char *str)
+{
+    char *newstr = new (std::nothrow) char[strlen(str) + 1];
+    if (!newstr)
+    {
+        std::cout << " Cannot allocate memory ";
+        return nullptr;
+    }
+    strcpy(newstr, str);
+    return newstr;
+}
+
+void MyString::copy(const MyString &other)
+{
+    this->string = new (std::nothrow) char[other.length + 1];
+    if (this->string)
+    {
+        strcpy(string, other.string);
+        this->length = other.length;
+    }
+    else
+    {
+        std::cout << " Cannot allocate memory";
+    }
+}
 
 MyString::MyString()
 {
-    this->string=nullptr;
-    this->length=0;
+    this->string = nullptr;
+    this->length = 0;
 }
 
-MyString::MyString(const char* str)
+MyString::MyString(const char *str)
 {
-    
+    this->string = dynString(str);
+    this->length = strlen(string);
+}
+
+MyString::MyString(const MyString &other)
+{
+    this->copy(other);
+}
+MyString::~MyString()
+{
+    this->deallocate();
+}
+
+void MyString::setString(const char *str)
+{
+    this->string = dynString(str);
+    this->length = strlen(str);
+}
+
+const char *MyString::getString() const
+{
+    return this->string;
+}
+
+size_t MyString::getLength() const
+{
+    return this->length;
+}
+
+char &MyString::at(size_t pos)
+{
+    if (pos >= this->length)
+    {
+        throw std::out_of_range("This position does not exist");
+    }
+    else
+    {
+        return this->string[pos];
+    }
+}
+
+const char &MyString::at(size_t pos) const
+{
+    if (pos >= this->length)
+    {
+        throw std::out_of_range("This position does not exist");
+    }
+    else
+    {
+        return this->string[pos];
+    }
+}
+
+char &MyString::operator[](size_t pos)
+{
+    assert(pos < this->length);
+    return this->string[pos];
+}
+
+const char &MyString::operator[](size_t pos) const
+{
+    assert(pos < this->length);
+    return this->string[pos];
+}
+
+char &MyString::front()
+{
+    assert(this->empty());
+    return this->string[0];
+}
+
+const char &MyString::front() const
+{
+    assert(this->empty());
+    return this->string[0];
+}
+
+char &MyString::back()
+{
+    assert(this->empty());
+    return this->string[this->length - 1];
+}
+
+const char &MyString::back() const
+{
+    assert(this->empty());
+    return this->string[this->length - 1];
+}
+
+bool MyString::empty() const
+{
+    return !length;
+}
+
+void MyString::clear()
+{
+    this->deallocate();
+    this->length = 0;
+}
+
+void MyString::push_back(char c)
+{
+    char temp[this->length + 1];
+    strcpy(temp, string);
+    this->length++;
+    temp[this->length - 1] = c;
+    *string = *temp;
+    delete[] temp;
+}
+
+void MyString::printString() const
+{
+    for (int i = 0; i < this->length; i++)
+    {
+        std::cout << string[i];
+    }
+    std::cout << std::endl;
 }
--- Homework/MyString.h
+++ Homework/MyString.h
@@ -4,49 +4,50 @@
 
 class MyString
 {
-    private:
-    char* string;
+private:
+    char *string;
     size_t length;
+    void deallocate();
+    char *dynString(const char *str);
+    void copy(const MyString &other);
 
-
-    public:
+public:
     //constructors
     MyString();
-    MyString (const char* str);
+    MyString(const char *str);
     MyString(const MyString &other);
 
     //Setters
-    void setString(const char* str);
-    void setLength(const char* length);
+    void setString(const char *str);
 
     //Getters
-    int getString() const;
-    int getLength()const;
+    const char *getString() const;
+    size_t getLength() const;
 
     //Functions
-    char& at (size_t pos);
-    const char& at(size_t pos) const;
-    char& operator[](size_t pos);
-    const char& operator[](size_t pos) const;
-    char& front();
-    const char& front() const;
-    const char& back() const;
+    char &at(size_t pos);
+    const char &at(size_t pos) const;
+    char &operator[](size_t pos);
+    const char &operator[](size_t pos) const;
+    char &front();
+    const char &front() const;
+    char &back();
+    const char &back() const;
     bool empty() const;
-    std::size_t size() const;
     void clear();
     void push_back(char c);
-    void pop_back();
-    MyString& operator+=(char c);
-    MyString& operator+=(const MyString& rhs);
-    MyString operator+(char c) const;
-    MyString operator+(const MyString& rhs) const;
-    const char* c_str() const;
-    bool operator==(const MyString &rhs) const;
-    bool operator<(const MyString &rhs) const;
+    void printString() const;
+    // void pop_back();
+    // MyString &operator+=(char c);
+    // MyString &operator+=(const MyString &rhs);
+    // MyString operator+(char c) const;
+    // MyString operator+(const MyString &rhs) const;
+    // const char *c_str() const;
+    // bool operator==(const MyString &rhs) const;
+    // bool operator<(const MyString &rhs) const;
 
     //Destructor
     ~MyString();
-
 };
 
 #endif
